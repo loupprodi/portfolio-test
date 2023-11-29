523a0c129d83d2820ea122c4e560e42a45d9ef3a
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-11-29 17:28:59 -0300

    adjust container in page 404

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,15 +2,17 @@ import { Container } from "../../components/Container"
 import { Footer } from "../../components/Footer"
 import { Header } from "../../components/Header"
 
+import styles from "./Page404.module.css"
+
 export const Page404 = () =>{
     return(
         <>
         <Header />
             <Container>
-                <h2>Algo de errado nao está certo!</h2>
-                <div>
-                    <span>404</span> <br />
-                    <strong>Página Não Localizada!</strong>
+                <h2 className={styles.titulo2}>Algo de errado nao está certo!</h2>
+                <div className={styles.textos}>
+                    <span className={styles.texto_grande}>404</span> <br />
+                    <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
                 </div>
             </Container>
         <Footer />
